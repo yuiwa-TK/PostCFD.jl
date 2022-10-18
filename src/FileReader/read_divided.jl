4a2a77a40b3d9a4c8815905974603e6ed9210330
Yuta Iwatani <yuta.iwatani.cfd@gmail.com>
2022-10-18 11:02:49 +0900

    Fix read_dividedsnapshots

--- src/FileReader/read_divided.jl
+++ src/FileReader/read_divided.jl
@@ -1,12 +1,12 @@
 """
-    read_dividedrestart(Compdir::String,flowids::Union{OrdinalRange,Vector{Int}})
+    read_dividedrestart(Compdir::AbstractString,flowids::Union{OrdinalRange,Vector{Int}})
 
 read divided restart files.
 This function returns xyz_vector=Vector{Array{Float64}(jmax,kmax,lmax,3)}.
 Arg1 : name of directory that contains the divided restart files.
 Arg2 : flow rank(number) indicated by range (eg. 1:2:19) or array (e.g. [1,3,5,⋯,19]) where you will read.
 """
-function read_dividedrestart(Compdir::String,flowids::Union{OrdinalRange,Vector{Int}})
+function read_dividedrestart(Compdir::AbstractString,flowids::Union{OrdinalRange,Vector{Int}})
     # OrdinalRange includes typeof(ns:ne) and typeof(ns:nskip:ne) 
     # flowids=ns:nskip:ne
     
@@ -22,7 +22,7 @@ function read_dividedrestart(Compdir::String,flowids::Union{OrdinalRange,Vector{
     end
     return vec_flow
 end
-function read_dividedrestart(Compdir::String,flowids::Union{OrdinalRange,Vector{Int}},filebase::String)
+function read_dividedrestart(Compdir::AbstractString,flowids::Union{OrdinalRange,Vector{Int}},filebase::AbstractString)
     vec_flow = Vector{Array{AbstractFloat}}(undef,length(flowids))
     count = 0
     for n in flowids
@@ -35,17 +35,26 @@ function read_dividedrestart(Compdir::String,flowids::Union{OrdinalRange,Vector{
     return vec_flow
 end
 
-function read_dividedsnapshot(Compdir::String,flowids::Union{OrdinalRange,Vector{Int}},
-                            filebase::String,suffix::String)
+function read_dividedsnapshot(Compdir::AbstractString,flowids::Union{OrdinalRange,Vector{Int}},steps::Union{OrdinalRange,Vector{Int}},
+                            filebase::AbstractString,suffix::AbstractString;precision::AbstractString)
 
-    vec_flow =  Vector{Array{AbstractFloat}}(undef,length(flowids))
+    vec_flow =  Vector{Array{AbstractFloat}}(undef,length(flowids)*length(steps))
+    if precision=="double"
+        my_read = read_flow_double
+    elseif precision=="single"
+        my_read = read_flow_single
+    end
+    
     count = 0
-    for n in flowids
-        divide_num  = @sprintf "%8.8i"  n
-        flowfile    = Compdir*filebase*divide_num
-        println("read file ",flowfile)
-        count+=1
-        vec_flow[count]=read_restart(flowfile)
+    for s     in steps
+    for irank in flowids
+        @info  s, irank
+        count      += 1
+        srank       = @sprintf "%5.5i"  irank
+        sstep       = @sprintf "%8.8i"  s 
+        flowfile    = Compdir*filebase*srank*"_"*sstep*suffix
+        vec_flow[count]=my_read(flowfile)
+    end
     end
     return vec_flow
 end
@@ -56,7 +65,7 @@ This function returns xyz_vector=Vector{Array{Float64}(jmax,kmax,lmax,3)}.
 Arg1 : name of directory that contains the divided grid files.
 Arg2 : grid rank(number) indicated by range (eg. 1:2:19) or array (e.g. [1,3,5,⋯,19]) where you will read.
 """
-function read_dividedgrid(Compdir::String,gridids::Union{OrdinalRange,Vector{Int}})
+function read_dividedgrid(Compdir::AbstractString,gridids::Union{OrdinalRange,Vector{Int}})
     # OrdinalRange includes typeof(ns:ne) and typeof(ns:nskip:ne) 
     # gridids=ns:nskip:ne
     vec_xyz = Vector{Array{AbstractFloat}}(undef,length(gridids))
