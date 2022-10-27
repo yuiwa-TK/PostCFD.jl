12d7ee6de2a60dc94e2970a11d92438344b3309f
Yuta Iwatani <yuta.iwatani.cfd@gmail.com>
2022-10-27 20:48:32 +0900

    Add read uns

---
+++ src/FileReader/read_uns.jl
@@ -0,0 +1,51 @@
+function read_uns(filename::AbstractString)
+    ngridpoints=Vector{Int32}(undef,1)
+    uidum   = Vector{UInt8}(undef,80)
+    idum1   = Vector{Int32}(undef,1)
+    idum2   = Vector{Int32}(undef,2)
+    idum4   = Vector{Int32}(undef,4)
+    rdum4   = Vector{Float32}(undef,4)
+    nvar=Vector{Int32}(undef,1)
+    qall = 0
+
+
+    open(filename,"r") do io
+        read!(io,idum1)
+        a=read!(io,uidum)
+        String(a)
+        read!(io,idum4)
+        read!(io,rdum4)
+        read!(io,idum1)
+        @show read!(io,nvar)
+        dumstr = Vector{UInt8}(undef,80*nvar[1]) #namelist of function
+        aa=read!(io,dumstr)
+        read!(io,idum2)
+        @show read!(io,ngridpoints)
+        qall = Array{Float32}(undef,(nvar[1],ngridpoints[1]))
+        read!(io,qall)
+        read!(io,idum1)
+    end
+    return qall
+end
+
+
+# #IN Fortran 
+# open(32, file= trim(File_field), form = "unformatted",access="stream")
+# read(32) FV_MAGIC
+# read(32) FV_txt
+# read(32) dum1, dum2, FV_RESULTS_FILE, dum3
+# print*, dum1,dum2,FV_RESULTS_FILE,dum3
+# read(32) rdum1,rdum2,rdum3,rdum4
+# print*, rdum1,rdum2,rdum3,rdum4
+# read(32) ngrid
+# read(32) nvar
+# allocate(FV_var(nvar))
+# do i=1,nvar
+#    read(32) FV_var(i)
+# enddo
+# read(32)FV_NODES, npt
+# read(32) FV_VARIABLES
+# allocate(qp(nvar,FV_VARIABLES))
+# read(32) ((qp(i,n), i=1,nvar),n=1,FV_VARIABLES)
+# read(32) FV_BNDRY_VARS
+# close(32)
