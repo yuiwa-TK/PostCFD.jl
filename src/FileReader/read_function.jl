7c1c8deae2c94b6c06290a7922d8d510808e8a76
Yuta Iwatani <yuta.iwatani.cfd@gmail.com>
2023-10-28 20:53:10 +0900

    implement read the specified function data

--- src/FileReader/read_function.jl
+++ src/FileReader/read_function.jl
@@ -42,6 +42,26 @@ function read_function_dims(filename::String)
     return dims
 end
 
+
+function typeof_functionfile(filename::AbstractString)
+    Nb_INT32 = 4
+    NBF_FLOAT64 = 8
+    NBF_FLOAT32 = 4
+    Nvars = prod(read_function_dims(filename))
+    Nb_file = filesize(filename)
+
+    if Nb_file == 4*Nb_INT32 + Nvars*NBF_FLOAT32
+        println("$filename is single format")
+        return "single"
+    elseif Nb_file == 4*Nb_INT32 + Nvars*NBF_FLOAT64
+        println("$filename is double format")
+        return "double"
+    else
+        @error println("$filename is not written in pl3d format or written with record marker .")
+    end
+    return 0
+end
+
 """
     read_flow_auto(filename::AbstractString)
 automatically determines the file type written in pl3d format.
@@ -63,3 +83,34 @@ function read_function_auto(filename::AbstractString)
     end
 end
 read_function=read_function_auto
+
+
+function read_function_specifyingvaribale(filename::String,idvar::Int; verbose=2)
+    if verbose>=1
+        @show filename
+    end
+    tp = typeof_functionfile(filename)
+    dims = Array{Int32}(undef,(4))
+
+    if tp=="single"
+        qvar = Array{Float32}(undef,(dims[1],dims[2],dims[3]))
+        io   = open(filename,"r") 
+        read!(io,dims)
+        Nb_skip = prod(@view dims[1:3])*(idvar-1)
+        skip(io,Nb_skip)
+        read!(io,qvar)
+        close(io)
+        return qvar
+    elseif tp=="double"
+        qvar = Array{Float64}(undef,(dims[1],dims[2],dims[3]))
+        io   = open(filename,"r") 
+        read!(io,dims)
+        Nb_skip = prod(@view dims[1:3])*(idvar-1)
+        skip(io,Nb_skip)
+        read!(io,qvar)
+        close(io)
+        return qvar
+    else
+        return nothing
+    end
+end
