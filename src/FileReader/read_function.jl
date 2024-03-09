c48a409532b42468b4943f5e63fa06e7bdeca1c8
Yuta Iwatani <yuta.iwatani.cfd@gmail.com>
2024-03-09 23:49:15 +0900

    enable to read big enidian formatted data if endian ="big"

--- src/FileReader/read_function.jl
+++ src/FileReader/read_function.jl
@@ -74,14 +74,21 @@ function typeof_functionfile(filename::AbstractString; verbose=2)
 end
 
 """
-    read_flow_auto(filename::AbstractString)
+    read_flow_auto(filename::AbstractString; verbose=2, endian="little")
 automatically determines the file type written in pl3d format.
+
+# input
+- endian = "little"/"big"
+- verbose = 2(show filename), 0(no output)
 """
-function read_function_auto(filename::AbstractString; verbose=2)
+function read_function_auto(filename::AbstractString; verbose=2,endian="little")
     Nb_INT32 = 4
     NBF_FLOAT64 = 8
     NBF_FLOAT32 = 4
     Nvars = prod(read_function_dims(filename;verbose=0))
+    if endian!="little"
+        Nvars = prod(ntoh.(read_function_dims(filename;verbose=0)))
+    end
     Nb_file = filesize(filename)
 
     if Nb_file == 4*Nb_INT32 + Nvars*NBF_FLOAT32
@@ -95,19 +102,26 @@ function read_function_auto(filename::AbstractString; verbose=2)
 end
 read_function=read_function_auto
 
-
-function read_function_specifyingvaribale(filename::String,idvar::Int; verbose=2)
+"""
+    read_function_specifyingvaribale(filename::String,idvar::Int; verbose=2,endian="little")
+# input
+- endian = "little"/"big"
+"""
+function read_function_specifyingvaribale(filename::String,idvar::Int; verbose=2,endian="little")
     NBF_FLOAT64 = 8
     NBF_FLOAT32 = 4
     if verbose>=1
         @show filename
     end
-    tp = typeof_functionfile(filename; verbose=0)
+    tp = typeof_functionfile(filename; verbose=0,endian=endian)
     dims = Array{Int32}(undef,(4))
 
     if tp=="single"
         io   = open(filename,"r") 
         read!(io,dims)
+        if endian!="little"
+            dims=ntoh.(dims)
+        end
         qvar = Array{Float32}(undef,(dims[1],dims[2],dims[3]))
         Nb_skip = prod(@view dims[1:3])*(idvar-1)*NBF_FLOAT32
         skip(io,Nb_skip)
@@ -117,6 +131,9 @@ function read_function_specifyingvaribale(filename::String,idvar::Int; verbose=2
     elseif tp=="double"
         io   = open(filename,"r") 
         read!(io,dims)
+        if endian!="little"
+            dims=ntoh.(dims)
+        end
         qvar = Array{Float64}(undef,(dims[1],dims[2],dims[3]))
         Nb_skip = prod(@view dims[1:3])*(idvar-1)*NBF_FLOAT64
         skip(io,Nb_skip)
@@ -135,18 +152,21 @@ reads the value of specified veriable and at l index.
 ## i.e.)
     read_function_specifying_l_and_variable(file, 10, 1) == read_function_auto(file)[:,:,10,1]
 """
-function read_function_specifying_l_and_variable(filename::String,lid::Int, idvar::Int; verbose=2)
+function read_function_specifying_l_and_variable(filename::String,lid::Int, idvar::Int; verbose=2,endian="little")
     NBF_FLOAT64 = 8
     NBF_FLOAT32 = 4
     if verbose>=1
         @show filename
     end
-    tp = typeof_functionfile(filename; verbose=0)
+    tp = typeof_functionfile(filename; verbose=0, endian=endian)
     dims = Array{Int32}(undef,(4))
 
     if tp=="single"
         io   = open(filename,"r") 
         read!(io,dims)
+        if endian!="little"
+            dims=ntoh.(dims)
+        end
         qvar = Array{Float32}(undef,(dims[1],dims[2],1))
         
         # skip bytes for variable
@@ -158,10 +178,17 @@ function read_function_specifying_l_and_variable(filename::String,lid::Int, idva
         skip(io,Nb_skip2)
         read!(io,qvar)
         close(io)
-        return qvar
+        if endian=="little"
+            return qvar
+        elseif endian=="big"
+            return ntoh.(qvar)
+        end
     elseif tp=="double"
         io   = open(filename,"r") 
         read!(io,dims)
+        if endian!="little"
+            dims=ntoh.(dims)
+        end
         qvar = Array{Float64}(undef,(dims[1],dims[2],dims[3]))
 
         # skip bytes for variable
@@ -173,7 +200,11 @@ function read_function_specifying_l_and_variable(filename::String,lid::Int, idva
         skip(io,Nb_skip2)
         read!(io,qvar)
         close(io)
-        return qvar
+        if endian=="little"
+            return qvar
+        elseif endian=="big"
+            return ntoh.(qvar)
+        end
     else
         return nothing
     end
