32fb30b5820693c369f5b89b0a724cae3d12695c
Yuta Iwatani <yuta.iwatani.cfd@gmail.com>
2024-10-26 19:34:07 +0900

    add flatness and skewness with Favere averaging

---
+++ src/VarsConverter/flatness.jl
@@ -0,0 +1,25 @@
+
+
+function flatness(f,ff,fff,ffff; is_normalize=true)
+    
+    if is_normalize
+        d = ff .- f.*f
+    else
+        d = 1.0
+    end
+    return (ffff .- 4.0.*fff.*f .+ 6.0.*ff.*f.*f .- 3.0.*f.*f.*f.*f)./d
+
+end
+
+
+function flatness(r,rf,rff,rfff,rffff; is_normalize=true)
+    
+    f_fave = rf./r
+    if is_normalize
+        d = rff .- r.*f_fave.*f_fave
+    else
+        d = 1.0
+    end
+    return (rffff .- 4.0.*rfff.*f_fave .+ 6.0.*rff.*f_fave.*f_fave .- 3.0.*f_fave.*f_fave.*f_fave.*f_fave)./d
+
+end
--- src/VarsConverter/skewness.jl
+++ src/VarsConverter/skewness.jl
@@ -9,7 +9,15 @@ function skewness(f, ff, fff; is_normalize=true)
     else
         d=1.0
     end
-    return (fff .- 3.0.*ff.*f + 2.0.*f.*f.*f)./d
+    return (fff .- 3.0.*ff.*f .+ 2.0.*f.*f.*f)./d
+end
+
+"""
+skewness (f, g, fg, ff, ffg)
+computes the cross skewness ⟨f'f'g'⟩
+"""
+function skewness(f, g, fg, ff, fgh)
+    return fgh .- 2.0.*f.*fg .- g.*ff .+ 2.0.*f.*g.*f
 end
 
 """
@@ -17,5 +25,40 @@ skewness (f, g, h, fg, gh, hf, fgh)
 computes the cross skewness ⟨f'g'h'⟩
 """
 function skewness(f, g, h, fg, gh, hf, fgh)
-    return fgh .- f.*gh .-g.*hf .- h.*fg + 2.0.*f.*g.*h
+    return fgh .- f.*gh .-g.*hf .- h.*fg .+ 2.0.*f.*g.*h
+end
+
+"""
+skewness (r, rf, rff, rfff; is_normalize=true)
+computes the Favre-averaged skewness ⟨rf'f'f'⟩/⟨rf'f'⟩^{1.5} (if is_normalize is true).
+"""
+function skewness(r, rf, rff, rfff; is_normalize=true)
+
+    f_fave = rf./r
+
+    if is_normalize
+        variance = rff .- r.*f_fave.*f_fave
+        d = variance.*sqrt.(abs.(variance))
+        return (rfff .- 3.0.*rff.*f_fave .+ 3.0.*rf.*f_fave .- r.*f_fave.*f_fave.*f_fave)./d
+    else
+        return (rfff .- 3.0.*rff.*f_fave .+ 3.0.*rf.*f_fave .- r.*f_fave.*f_fave.*f_fave)./r
+    end
+    return (fff .- 3.0.*ff.*f + 2.0.*f.*f.*f)./d
+end
+
+"""
+skewness (r, rf, rff, rfff; is_normalize=true)
+computes the Favre-averaged skewness {f''f''g''} = ⟨rf'f'g'⟩/⟨r⟩
+"""
+function skewness(r, rf, rg, rff, rfg, rffg)
+
+    rinv = 1.0./r
+    f_fave = rf.*rinv
+    g_fave = rg.*rinv
+    v1 = rinv.*rffg
+    v2 = rinv.*rfg.*f_fave
+    v3 = rinv.*rff.*g_fave
+    v4 = rinv.*rf.*f_fave.*g_fave
+
+    return v1 .- 2.0.*v2 .- v3 .+2.0.*v4
 end
