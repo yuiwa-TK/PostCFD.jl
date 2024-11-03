c8997ecc26bdca3c953c164fec2a3a2dfe4b5b29
Yuta Iwatani <yuta.iwatani.cfd@gmail.com>
2024-11-03 16:35:55 +0900

    debug

--- src/Math/integrals.jl
+++ src/Math/integrals.jl
@@ -1,4 +1,16 @@
-function ∫fdy(f,y; intrange, type="trapezoid")
+function ∫fdy(f,y; intrange, type="trapezoid_extp")
+    if type=="trapezoid"
+        return ∫fdy_tp(f,y; intrange)
+    elseif type=="trapezoid_extp"
+        return ∫fdy_tpex(f,y; intrange)
+    end
+end
+
+"""
+∫fdy_tp(f,y; intrange)
+computes the integral function value `f` within `intrange` with trapezoid approximation.
+"""
+function ∫fdy_tp(f,y; intrange)
     zs   = filter(y-> intrange[1]<=y<=intrange[2], y)
     if length(zs)<2
         @warn "length(zs<2). 0 is returned."
@@ -16,7 +28,12 @@ function ∫fdy(f,y; intrange, type="trapezoid")
     end
 end
 
-function ∫fdy(f,y; intrange, type="trapezoid_extp")
+
+"""
+∫fdy_tpex(f,y; intrange)
+computes the integral function value `f` within `intrange` with trapezoid approximation, padding the loss the gap between intrange[2] and y[le] where le is the greatest integer satisfying y[le]<=intrange[2] using a linear intporation.
+"""
+function ∫fdy_tpex(f,y; intrange)
     zs   = filter(y-> intrange[1]<=y<=intrange[2], y)
     if length(zs)<2
         @warn "length(zs<2). 0 is returned."
@@ -33,7 +50,6 @@ function ∫fdy(f,y; intrange, type="trapezoid_extp")
         le = findfirst(j -> intrange[2] < y[j+1], 1:length(y)) # y[le]<=intrange[2]
         if le<length(y)
             extp = (intrange[2]-y[le]) / (y[le+1]-y[le])* (y[le+1]-y[le]) * 0.5*(f[le]+f[le+1])
-            # @show r = (intrange[2]-y[le]) / (y[le+1]-y[le])
             return sumv + extp
         else
             return sumv
